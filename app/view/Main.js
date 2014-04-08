f3747435a587e11a68a53ccfcda6c7718d98259a
Felipe Villa <felipe.villa@bleext.com>
2014-04-08 10:51:04 -0500

    use slidenavigation as a main view. added style to slidenavigation

--- app/view/Main.js
+++ app/view/Main.js
@@ -1,50 +1,27 @@
-Ext.define('Pulso.view.Main', {
-    extend: 'Ext.tab.Panel',
+Ext.define("Pulso.view.Main", {
+    extend: 'Ext.ux.slidenavigation.View',
     xtype: 'main',
-    requires: [
-        'Ext.TitleBar',
-        'Ext.Video'
-    ],
+    
     config: {
-        tabBarPosition: 'bottom',
-
-        items: [
-            {
-                title: 'Welcome',
-                iconCls: 'home',
-
-                styleHtmlContent: true,
-                scrollable: true,
-
-                items: {
-                    docked: 'top',
-                    xtype: 'titlebar',
-                    title: 'Welcome to Sencha Touch 2'
-                },
-
-                html: [
-                    "You've just generated a new Sencha Touch 2 project. What you're looking at right now is the ",
-                    "contents of <a target='_blank' href=\"app/view/Main.js\">app/view/Main.js</a> - edit that file ",
-                    "and refresh to change what's rendered here."
-                ].join("")
-            },
-            {
-                title: 'Get Started',
-                iconCls: 'action',
-
-                items: [
-                    {
-                        docked: 'top',
-                        xtype: 'titlebar',
-                        title: 'Getting Started'
-                    },
-                    {
-                        xtype: 'video',
-                        url: 'http://av.vimeo.com/64284/137/87347327.mp4?token=1330978144_f9b698fea38cd408d52a2393240c896c',
-                        posterUrl: 'http://b.vimeocdn.com/ts/261/062/261062119_640.jpg'
-                    }
-                ]
-            }
-        ]
+        slideSelector: false,
+        containerSlideDelay: 10,
+        selectSlideDuration: 200,
+        listPosition: 'left',
+        list: {
+            maxDrag: 200,
+            width: 50,
+            grouped: false,
+            items: [{
+                xtype: 'toolbar',
+                docked: 'top',
+                ui: 'dark'
+            }]
+            
+        },
+        items           : [{
+            xtype   : 'container',
+        },{
+            xtype   : 'container',
+        }]
     }
 });
